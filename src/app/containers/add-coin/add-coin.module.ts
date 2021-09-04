f006e0cfb25fe852e479e481184489aa669711d1
Andres Guerrero <andresmgsl@gmail.com>
2021-09-05 00:36:45 +0200

    feat: :fire: Creating basic containers

---
+++ src/app/containers/add-coin/add-coin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddCoinComponent } from './add-coin.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddCoinComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddCoinModule { }
