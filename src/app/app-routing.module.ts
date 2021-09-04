9656b41b944af4b430bf2a5573500f94becd7b7b
Andres Guerrero <andresmgsl@gmail.com>
2021-09-05 00:47:11 +0200

    feat: :zap: Adding routes for containers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./containers/home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'add-new-coin',
+        loadChildren: () =>
+          import('./containers/add-coin/add-coin.module').then(
+            (m) => m.AddCoinModule
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./containers/profile/profile.module').then(
+            (m) => m.ProfileModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
